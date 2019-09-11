84411e6c4653e35b0fb745296d703572f96df6ba
kanmanigayu <knambi45@gmail.com>
2019-09-11 01:06:06 -0700

    Empolyee Info

---
+++ src/org/sample/EmpDetails.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+
+public class EmpDetails {
+ private void empId() {
+System.out.println(" emp name is kanmani");
+
+
+}
+public static void main(String[] args) {
+	EmpDetails a =new EmpDetails();
+	a.empId();
+}
+}
